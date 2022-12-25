a5f316668207037312898965b5a116534bec492d
ravgw <raf.gwizdak@gmail.com>
2022-12-25 12:55:16 +0100

    bolid part 1 - body

--- displaying-elements.js
+++ displaying-elements.js
@@ -2,11 +2,13 @@
 export const createCarClicker = function () {
 const clicker = document.getElementById('clicker');
 
-const carBody = document.createElement('p');
-carBody.id = 'carBody';
+const carBodyRear = document.createElement('p');
+carBodyRear.id = 'carBodyRear';
+const carBodyFront = document.createElement('p');
+carBodyFront.id = 'carBodyFront';
 const rearTyre = document.createElement('p');
 rearTyre.classList.add('tyres');
 
-clicker.appendChild(carBody);
-// clicker.appendChild(body);
-} 
+clicker.appendChild(carBodyRear);
+clicker.appendChild(carBodyFront);
+}  
