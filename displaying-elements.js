5e20a103724caffb4702ccda5d2344af3a115664
ravgw <raf.gwizdak@gmail.com>
2022-12-28 22:03:01 +0100

    bolid part 2 - tyres rotating bug

--- displaying-elements.js
+++ displaying-elements.js
@@ -6,9 +6,22 @@ const carBodyRear = document.createElement('p');
 carBodyRear.id = 'carBodyRear';
 const carBodyFront = document.createElement('p');
 carBodyFront.id = 'carBodyFront';
+const  door = document.createElement('p');
+door.id = 'door';
 const rearTyre = document.createElement('p');
 rearTyre.classList.add('tyres');
+const rearRim = document.createElement('p');
+rearRim.classList.add('rim');
+const frontTyre = document.createElement('p');
+frontTyre.classList.add('tyres');
+const frontRim = document.createElement('p');
+frontRim.classList.add('rim');
 
 clicker.appendChild(carBodyRear);
 clicker.appendChild(carBodyFront);
+carBodyRear.appendChild(door);
+carBodyRear.appendChild(rearTyre);
+rearTyre.appendChild(rearRim);
+carBodyFront.appendChild(frontTyre);
+frontTyre.appendChild(frontRim);
 }  
