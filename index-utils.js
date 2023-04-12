ccfbfb9ca6546e55e0a6be992a15227a7b2a5f6e
ravgw <raf.gwizdak@gmail.com>
2023-04-12 22:26:08 +0200

    menu -> information box buttons / & animation

--- index-utils.js
+++ index-utils.js
@@ -16,4 +16,32 @@ function addHover () {
     });
 }
 
+function showInformationBox () {
+
+    const menu = document.querySelector('.menu__board')
+    menu.classList.add('hide-menu')
+    
+    setTimeout(() => {
+        menu.style.display = 'none'
+        menu.classList.remove('hide-menu')
+        showBox()
+    }, 300)
+
+    function showBox () {
+        document.querySelector('.information-box').style.display = 'flex'
+    }
+}
+
 addHover()
+
+const newGame = document.querySelector('#menu__new-game')
+newGame.addEventListener('click', () => {
+    console.log('new game')
+})
+
+const accessoryButton = document.querySelectorAll('.menu__accessory button')
+accessoryButton.forEach( e => {
+    e.addEventListener('click', () => {
+        showInformationBox()
+    })
+})
