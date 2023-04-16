a0ef3ed6b9f8ba02b0544fcdd6825a9cedcb3d6a
ravgw <raf.gwizdak@gmail.com>
2023-04-16 20:47:07 +0200

    set menu /accessory function

--- index-utils.js
+++ index-utils.js
@@ -16,23 +16,44 @@ function addHover () {
     });
 }
 
-function showInformationBox () {
+function setAccessory () {
 
     const menu = document.querySelector('.menu__board')
-    menu.classList.add('hide-menu')
+    menu.classList.add('hide-element')
     
     setTimeout(() => {
         menu.style.display = 'none'
-        menu.classList.remove('hide-menu')
-        showBox()
+        menu.classList.remove('hide-element')
+        showAccessory()
     }, 300)
-
-    function showBox () {
-        document.querySelector('.accessory-box').style.display = 'flex'
+    
+    function showAccessory () {
+        document.querySelector('.accessory__box').style.display = 'flex'
+        backButton.style.display = 'flex'
     }
 }
 
-addHover()
+function setMenu  () {
+    const accessory = document.querySelector('.accessory__box')
+    accessory.classList.add('hide-element')
+    backButton.classList.add('hide-element')
+
+    setTimeout(() => {
+        accessory.style.display = 'none'
+        accessory.classList.remove('hide-element')
+        backButton.classList.remove('hide-element')
+        showMenu()
+    }, 300)
+
+    function showMenu () {
+        document.querySelector('.menu__board').style.display = 'flex'
+        backButton.style.display = 'none'
+    }
+}
+
+
+
+// ___________________________ LISTENERS_______________________________
 
 const newGame = document.querySelector('#menu__new-game')
 newGame.addEventListener('click', () => {
@@ -42,6 +63,16 @@ newGame.addEventListener('click', () => {
 const accessoryButton = document.querySelectorAll('.menu__accessory button')
 accessoryButton.forEach( e => {
     e.addEventListener('click', () => {
-        showInformationBox()
+        setAccessory()
     })
 })
+
+const backButton = document.querySelector('.accessory__box_back')
+backButton.addEventListener('click', () => {
+    setMenu()
+})
+
+
+
+addHover()
+
